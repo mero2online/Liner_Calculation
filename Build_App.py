d84a7eac5ca962f7504b868767099c2b527db81a
mero2online <mero2online@gmail.com>
2022-06-27 10:50:29 +0200

    EDIT: Change name in build file

--- Build_App.py
+++ Build_App.py
@@ -1,10 +1,10 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    'Cement_Calculation.py',
+    'Liner_Calculation.py',
     '--onefile',
     '--windowed',
     # '--add-data', 'src;src',
-    # '-i', ".\src\\Cement_Calculation.ico",
-    # '--splash', ".\src\\Cement_Calculation.png",
+    # '-i', ".\src\\Liner_Calculation.ico",
+    # '--splash', ".\src\\Liner_Calculation.png",
 ])
